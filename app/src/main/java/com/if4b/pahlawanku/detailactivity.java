9cee4b109a7689fed7f0132c544b2fc69adeac7c
Rgafril90 <93838735+Rgafril90@users.noreply.github.com>
2022-04-19 09:04:25 +0700

    commit
    >import file ke github

---
+++ app/src/main/java/com/if4b/pahlawanku/detailactivity.java
@@ -0,0 +1,14 @@
+package com.if4b.pahlawanku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailactivity);
+    }
+}
